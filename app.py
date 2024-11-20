93d3d7aa6850355457618da66260ea0f8fa1d860
Gaëtan <59415807+gaetanbrison@users.noreply.github.com>
2024-11-20 16:14:24 +0000

    new version of the app

--- app.py
+++ app.py
@@ -13,52 +13,77 @@ from sklearn.linear_model import LogisticRegression
 
 
 st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+
+page = st.sidebar.radio("Go to", ["Model", "Hyperparameter"])
+
+if page == "Model":
+    MODELS = {
+        "Logistic Regression":LogisticRegression,
+        "KNN":KNeighborsClassifier,
+        "Decision Tree":DecisionTreeClassifier,
+    }
+
+    model_mode=st.sidebar.selectbox("Select a model of your choice",['Logistic Regression','KNN','Decision Tree'])
+
+    df = pd.read_csv("churn-bigml-20.csv")
+    st.dataframe(df)
+
+
+    df.State=pd.Categorical(df['State']).codes
+    df['International plan']=pd.Categorical(df['International plan']).codes
+    df['Voice mail plan']=pd.Categorical(df['Voice mail plan']).codes
+    df['Churn']=pd.Categorical(df['Churn']).codes
 
 
 
-MODELS = {
-    "Logistic Regression":LogisticRegression,
-    "KNN":KNeighborsClassifier,
-    "Decision Tree":DecisionTreeClassifier,
-}
+    X = df.drop(["Churn"], axis=1)
+    y = df.Churn
 
-model_mode=st.sidebar.selectbox("Select a model of your choice",['Logistic Regression','KNN','Decision Tree'])
-
-df = pd.read_csv("churn-bigml-20.csv")
-st.dataframe(df)
+    X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.3)
 
 
-df.State=pd.Categorical(df['State']).codes
-df['International plan']=pd.Categorical(df['International plan']).codes
-df['Voice mail plan']=pd.Categorical(df['Voice mail plan']).codes
-df['Churn']=pd.Categorical(df['Churn']).codes
+    if model_mode == 'Logistic Regression':
+        log = LogisticRegression()
+        model = log.fit(X_train,y_train)
+        prediction = log.predict(X_test)
+        st.write("Accuracy Logistic Regression:",metrics.accuracy_score(y_test,prediction))
+
+    elif model_mode == 'KNN':
+        knn=KNeighborsClassifier(n_neighbors=3)
+        knn.fit(X_train,y_train)
+        prediction = knn.predict(X_test)
+        st.write("Accuracy KNN:",metrics.accuracy_score(y_test,prediction))
 
 
-
-X = df.drop(["Churn"], axis=1)
-y = df.Churn
-
-X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.3)
-
-
-if model_mode == 'Logistic Regression':
-    log = LogisticRegression()
-    model = log.fit(X_train,y_train)
-    prediction = log.predict(X_test)
-    st.write("Accuracy Logistic Regression:",metrics.accuracy_score(y_test,prediction))
-
-elif model_mode == 'KNN':
-    knn=KNeighborsClassifier(n_neighbors=3)
-    knn.fit(X_train,y_train)
-    prediction = knn.predict(X_test)
-    st.write("Accuracy KNN:",metrics.accuracy_score(y_test,prediction))
+    elif model_mode == 'Decision Tree':
+        tree=DecisionTreeClassifier()
+        tree.fit(X_train,y_train)
+        prediction = tree.predict(X_test)
+        st.write("Accuracy Tree:",metrics.accuracy_score(y_test,prediction))
 
 
-elif model_mode == 'Decision Tree':
-    tree=DecisionTreeClassifier()
-    tree.fit(X_train,y_train)
-    prediction = tree.predict(X_test)
-    st.write("Accuracy Tree:",metrics.accuracy_score(y_test,prediction))
+elif page=="Hyperparameter":
+    st.write("Select Optimal Model")
+
+    import dagshub
+    import mlflow
+    import streamlit_shadcn_ui as ui
+
+    dagshub.init("test1234","gaetan.brison",mlflow=True)
+    mlflow.start_run()
+
+    # training your model
+
+    mlflow.log_param("parameter name","value")
+    mlflow.log_metric("Accuracy",0.9)
+
+    mlflow.end_run()
+
+
+    st.title("ML Flow Visualization")
+
+    ui.link_button(text="👉 Go to ML Flow",url="https://dagshub.com/gaetan.brison/test1234.mlflow/#/",key="link_btnmlflow")
+
+    #https://dagshub.com/gaetan.brison/test1234.mlflow/#/
+
